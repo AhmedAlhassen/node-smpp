5c10cbc9af347da63c81dad53d6014079180cab5
Dave Horton <daveh@beachdognet.com>
2021-05-29 19:58:31 -0400

    major updates to ES6, promises

--- test/smpp.js
+++ test/smpp.js
@@ -2,38 +2,6 @@ var assert = require('assert'),
     fs = require('fs'),
     smpp = require('..');
 
-describe('Server', function() {
-	var server;
-
-	before(function() {
-		server = smpp.createServer();
-	});
-
-	describe('#listen()', function() {
-		beforeEach(function (done) {
-			server.listen(0, done);
-		});
-
-		afterEach(function (done) {
-			server.once('close', done);
-			server.close();
-		});
-
-		var port;
-
-		it('should bind to a random port', function() {
-			port = server.address().port;
-			assert.ok(port > 0, 'Invalid first random port');
-		});
-
-		it('should bind to another random port', function() {
-			var newPort = server.address().port;
-			assert.ok(newPort > 0, 'Invalid second random port');
-			assert.notEqual(newPort, port, 'Both random ports are equal!');
-		})
-	});
-});
-
 describe('Session', function() {
 	var server, port, secure = {};
 	var sessionHandler = function(session) {
@@ -44,17 +12,17 @@ describe('Session', function() {
 
 	before(function(done) {
 		server = smpp.createServer(sessionHandler);
-		server.listen(0, done);
+		server.listen(done);
 		port = server.address().port;
 	});
 
 	before(function(done) {
-		secure.server = smpp.createServer({
+		secure = smpp.createServer({
 			key: fs.readFileSync(__dirname + '/fixtures/server.key'),
 			cert: fs.readFileSync(__dirname + '/fixtures/server.crt')
 		}, sessionHandler);
-		secure.server.listen(0, done);
-		secure.port = secure.server.address().port;
+		secure.listen(done);
+		//secure.port = secure.address().port;
 	});
 
 	after(function(done) {
@@ -65,68 +33,37 @@ describe('Session', function() {
 	});
 
 	after(function(done) {
-		secure.server.sessions.forEach(function(session) {
+		secure.sessions.forEach(function(session) {
 			session.close();
 		});
-		secure.server.close(done);
+		secure.close(done);
 	});
 
-	describe('smpp.connect()', function() {
-		it('should use 2775 or 3550 as default port', function() {
-			var session = smpp.connect();
-			assert.equal(session.options.port, 2775);
-			var session = smpp.connect({tls: true});
-			assert.equal(session.options.port, 3550);
-			var session = smpp.connect('smpp://localhost');
-			assert.equal(session.options.port, 2775);
-			var session = smpp.connect('ssmpp://localhost');
-			assert.equal(session.options.port, 3550);
+	describe('smpp.connect()', async() => {
+		it('should successfully establish a connection', async() => {
+			const  session = await smpp.connect('smpp://localhost');
+			session.close();
 		});
 
-		it('should be backward compatible', function() {
-			var session = smpp.connect('127.0.0.1');
-			assert.equal(session.options.port, 2775);
-			assert.equal(session.options.host, '127.0.0.1');
-			var session = smpp.connect('127.0.0.1', 1234);
-			assert.equal(session.options.port, 1234);
-			assert.equal(session.options.host, '127.0.0.1');
-		});
-
-		it('should properly parse connection url', function() {
-			var session = smpp.connect('smpp://127.0.0.1:1234');
-			assert.equal(session.options.port, 1234);
-			assert.equal(session.options.host, '127.0.0.1');
-			var session = smpp.connect('ssmpp://localhost');
-			assert(session.options.tls);
-			var session = smpp.connect({ url: 'ssmpp://127.0.0.1:1234'});
-			assert(session.options.tls);
-			assert.equal(session.options.port, 1234);
-			assert.equal(session.options.host, '127.0.0.1');
-		});
-
-		it('should successfully establish a connection', function(done) {
-			var session = smpp.connect({ port: port }, done);
-		});
-
-		it('should successfully establish a secure connection', function(done) {
-			var session = smpp.connect({
-				port: secure.port,
-				tls: true,
-				rejectUnauthorized: false
-			}, done);
+		it('should successfully establish a secure connection', async() => {
+			const session = await smpp.connect('ssmpp://localhost');
+			session.close();
 		});
 	});
 
-	describe('#send()', function() {
-		it('should successfully send a pdu and receive its response', function(done) {
-			var session = smpp.connect({ port: port });
+	describe('#send()', () => {
+		it('should successfully send a pdu and receive its response', async() => {
+			var session = await smpp.connect('smpp://localhost');
+			console.log('connected');
 			var pdu = new smpp.PDU('enquire_link');
-			session.send(pdu, done.bind(this, null));
+			console.log('sending PDU');
+			const result = await session.send(pdu);
 		});
 
-		it('should successfully send a pdu using shorthand methods', function(done) {
-			var session = smpp.connect({ port: port, auto_enquire_link_period:10000 });
-			session.enquire_link(done.bind(this, null));
+		it('should successfully send a pdu using shorthand methods', async() => {
+			var session = await smpp.connect('smpp://localhost');
+			//var session = await smpp.connect({ port: port, auto_enquire_link_period:10000 });
+			const result = await session.enquire_link();
 		});
 	});
 });
